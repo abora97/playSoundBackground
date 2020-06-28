10280737b6bcb4240a4e7eb8172781f8a1845be9
Abora <https://github.com/abora97>
2020-06-28 14:13:14 +0200

    add some file

--- app/src/main/java/com/example/playsoundbackground/BackgroundSoundService.java
+++ app/src/main/java/com/example/playsoundbackground/BackgroundSoundService.java
@@ -1,4 +1,56 @@
 package com.example.playsoundbackground;
 
-class BackgroundSoundService {
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class BackgroundSoundService extends Service {
+
+    MediaPlayer mediaPlayer;
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+
+        Toast.makeText(this, "onBind", Toast.LENGTH_SHORT).show();
+
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+          mediaPlayer = MediaPlayer.create(this, R.raw.sound);
+             mediaPlayer.setLooping(true); // Set looping
+            mediaPlayer.setVolume(100, 100);
+            mediaPlayer.start();
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this, "onStartCommand", Toast.LENGTH_SHORT).show();
+
+             mediaPlayer.start();
+     //   Toast.makeText(getApplicationContext(), "Playing Bohemian Rashpody in the Background", Toast.LENGTH_SHORT).show();
+        return startId;
+    }
+
+
+
+
+    @Override
+    public void onDestroy() {
+      //  mediaPlayer.stop();
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
+      //    mediaPlayer.release();
+    }
+
+    @Override
+    public void onLowMemory() {
+    }
 }
--- app/src/main/java/com/example/playsoundbackground/MainActivity.java
+++ app/src/main/java/com/example/playsoundbackground/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.playsoundbackground;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnPlay;
+    MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnPlay = findViewById(R.id.btnPlay);
+        mp = MediaPlayer.create(this, R.raw.sound);
+
+
+        btnPlay.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Intent intent = new Intent(getApplicationContext(), BackgroundSoundService.class);
+        startService(intent);
+
+
+
     }
 }
